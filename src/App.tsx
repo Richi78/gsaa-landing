719e9cba6b091b0bc051364655069fa3d384540a
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-01-30 21:17:59 -0400

    delete unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import './App.css'
-import '@fontsource-variable/onest';
-import Video from './components/Video/Video.component'
-import BentoGrid from './components/BentoGrid/BentoGrid.component';
-import BentoItem from './components/BentoGrid/BentoItem.component';
+import "./App.css";
+import "@fontsource-variable/onest";
+import Video from "./components/Video/Video.component";
+import BentoGrid from "./components/BentoGrid/BentoGrid.component";
 
 function App() {
   return (
-    <div className='h-screen'>
+    <div className="h-screen">
       <Video />
       <main>
         <BentoGrid />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
